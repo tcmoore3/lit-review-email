2fdf335655bff0852f0e5ba16dc8bd8f04e9eaab
Tim Moore <tcmoore3@gmail.com>
2018-11-13 15:04:08 -0500

    Only include PDT line if a presenter is given

--- generate-email.py
+++ generate-email.py
@@ -6,12 +6,14 @@ td_tm = {'1': 'today', '2': 'tomorrow'}.get(td_tm, 'tomorrow')
 time = input('Enter the time [3pm]: ')
 if time == '':
     time = '3pm'
-loc_str = 'Select location for meeting.\n1 - Research Auditorium\n'
-loc_str += '2 - B520-1122\n3 - South Atrium\n4 - B16 B001E\n'
-loc_str += 'Choose from 1, 2, 3, 4 [1]: '
+locations = {'1': 'the Research Auditorium', '2': 'B520-1122',
+        '3': 'the South Atrium', '4': 'B16-B001E', '5': 'G063 & 064'}
+loc_str = 'Select location for meeting.\n'
+for n, loc in locations.items():
+    loc_str += '{} - {}\n'.format(n, loc)
+loc_str += 'Choose: '
 location = input(loc_str)
-location = {'1': 'the Research Auditorium', '2': 'B520-1122',
-        '3': 'the South Atrium', '4': 'B16-B001E'}.get(location, 'B520-1122')
+location = locations.get(location, 'B520-1122')
 presenter = input('Who is presenting?\nEnter presenter: ')
 title = input('Enter the title of the paper: ')
 link = input('Enter the url to the paper: ')
@@ -33,9 +35,13 @@ msg += '{} ({}/{}/{}) at {} '.format(td_tm, date.month, date.day,
 msg += 'in {}. '.format(location)
 msg += '{} will be presenting the following paper:\n\n'.format(presenter)
 msg += '{}\n{}\n\n'.format(title, link)
-msg += '{} will be presenting in the PDT after the '.format(pdt_presenter)
-msg += 'lit review, so stick around if you can give valuable feedback on '
-msg += 'their latest work.\n\nThe next few presenters are:\n'
+
+if pdt_presenter:
+    msg += '{} will be presenting in the PDT after the '.format(pdt_presenter)
+    msg += 'lit review, so stick around if you can give valuable feedback on '
+    msg += 'their latest work.\n\n'
+
+msg += 'The next few presenters are:\n'
 for person in next_speakers.split(','):
     msg += '{}\n'.format(person)
 msg += '\nLooking forward to seeing y\'all there.\nTim'
