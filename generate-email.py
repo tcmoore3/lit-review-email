788a864d2aedbb4b64e70add333179b6218cc0b1
Tim Moore <tcmoore3@gmail.com>
2020-06-08 23:36:57 -0400

    Some changes

--- generate-email.py
+++ generate-email.py
@@ -12,13 +12,17 @@ locations = collections.OrderedDict([
     ('2', 'B520-1122'),
     ('3', 'the South Atrium'),
     ('4', 'B16-B001E'),
-    ('5', 'G063 & 064')])
+    ('5', 'G063 & 064'),
+    ('6', 'B10-G065'),
+    ('7', 'Blue Jeans'),
+    ('8', 'Zoom'),
+    ])
 loc_str = 'Select location for meeting.\n'
 for n, loc in locations.items():
     loc_str += '{} - {}\n'.format(n, loc)
-loc_str += 'Choose [2]: '
+loc_str += 'Choose [8]: '
 location = input(loc_str)
-location = locations.get(location, 'B520-1122')
+location = locations.get(location, locations['8'])
 presenter = None
 while not presenter:
     presenter = input('Who is presenting?\nEnter presenter: ')
@@ -54,7 +58,7 @@ if pdt_presenter:
 
 msg += 'The next few presenters are:\n'
 for person in next_speakers.split(','):
-    msg += '{}\n'.format(person)
+    msg += '{}\n'.format(person.strip())
 msg += '\nLooking forward to seeing y\'all there.\nTim'
 print('\n')
 print(msg, '\n')
